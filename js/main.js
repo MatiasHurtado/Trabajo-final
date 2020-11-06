0137d2439af757130b946c7d454696cf491ba3a7
Matias <matias.wolcraft3@gmail.com>
2020-11-06 02:32:31 -0300

    implementado mapa haci chile

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@
 
     var regalo=document.getElementById('regalo');
     document.addEventListener("DOMContentLoaded" ,function(){
-        var map = L.map('mapa').setView([51.505, -0.09], 13);
+        var map = L.map('mapa').setView([-33.50891, -70.773239],20);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+L.marker([-33.50891, -70.773239]).addTo(map)
+    .bindPopup('GDLWebCamp 2018 <br> Boletos Ya')
     .openPopup();
         
-        //Campos Pases
+        //  Campos Pases
         var nombre = document.getElementById('nombre');
         var apellido = document.getElementById('apellido');
         var email = document.getElementById('email');
