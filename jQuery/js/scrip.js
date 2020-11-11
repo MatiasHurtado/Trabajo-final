4f03141daffd1608d571914710459d893145f3ca
Matias <matias.wolcraft3@gmail.com>
2020-11-10 23:27:27 -0300

    Probando jQuerys

---
+++ jQuery/js/scrip.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#contenido').append("con jQuery es mas facil")
+});
+
+document.addEventListener('DOMContentLoaded',function(event){
+    var elemento = document.getElementById('contenido2');
+    var parrafo = document.createElement('p');
+    var texto = document.createTextNode('Con javascript es mas dificil');
+    parrafo.appendChild(texto);
+    elemento.appendChild(parrafo);
+
+});
