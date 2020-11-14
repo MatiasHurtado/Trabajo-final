55a83f1a60fe804ced75a8e739f9fe03cb8030f7
Matias <matias.wolcraft3@gmail.com>
2020-11-14 14:51:46 -0300

    Documentacion js

--- jQuery/js/scrip.js
+++ jQuery/js/scrip.js
@@ -1,7 +1,9 @@
 //Este documento viene con el fin de evidenciar lo facil o lo que ahorra lineas de codigo al usar jQuery
+//Con Jquerys
 $(document).ready(function(){
     $('#contenido').append("con jQuery es mas facil")
 });
+//Javascrip puro
 document.addEventListener('DOMContentLoaded',function(event){
     var elemento = document.getElementById('contenido2');
     var parrafo = document.createElement('p');
